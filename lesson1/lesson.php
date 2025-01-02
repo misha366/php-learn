b9073d160db186914aaad699261883a96c7e54e4
misha366 <misha.moskalenko.jv@gmail.com>
2025-01-03 00:35:52 +0300

    added first lesson

---
+++ lesson1/lesson.php
@@ -0,0 +1,9 @@
+
+
+<?php
+
+// для того чтобы запустить скрипт, его нужно поместить в папку
+// xampp'a htdocs/mysite (переименовав в файл в index.php)
+// и открыть в браузере адрес http://localhost/mysite/
+
+echo "<h1>Hello world</h1>";

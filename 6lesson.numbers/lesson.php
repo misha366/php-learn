6fcf0dffbbd66d86598e1d65d535990f87264c95
misha366 <misha.moskalenko.jv@gmail.com>
2025-01-03 14:43:11 +0300

    added 6 lesson

---
+++ 6lesson.numbers/lesson.php
@@ -0,0 +1,17 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title><?= "Numbers"; ?></title>
+</head>
+<body>
+    <?php
+        $num = 12345;
+        $num_oct = 0123; # число, которое начинается с 0 в php измеряется
+        # в восьмеричной системе счисления
+        $num_hex = 0x12A3; # число, которое начинается с 0x в php измеряется
+        # в шестнадцатеричной системе счисления
+    ?>
+</body>
+</html>

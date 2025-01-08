147170ef83dc0a0c733c7101ad16625bf3065b46
misha366 <misha.moskalenko.jv@gmail.com>
2025-01-09 00:00:00 +0300

    added 20 lesson

---
+++ 20lesson.ifoperator/lesson.php
@@ -0,0 +1,17 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title><?= "If Operator"; ?></title>
+</head>
+<body>
+    <?php
+        if (/*condition*/) {
+            # code...
+        } else {
+            # code...
+        }        
+    ?>
+</body>
+</html>

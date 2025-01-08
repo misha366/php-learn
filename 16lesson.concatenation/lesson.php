cd4c98c6d7c860203f41061e88b1c4f913d3a2d2
misha366 <misha.moskalenko.jv@gmail.com>
2025-01-08 14:34:50 +0300

    added 16 lesson

---
+++ 16lesson.concatenation/lesson.php
@@ -0,0 +1,16 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title><?= "Concatenation"; ?></title>
+</head>
+<body>
+    <?php
+        // В пхп можно конкатенировать объекты с помощью операторов "." и  ".="
+
+        // Так же в функцию echo можно передать несколько операндов разделяя их комой,
+        // все они буду сконкатенированы и выведены на экран
+    ?>
+</body>
+</html>

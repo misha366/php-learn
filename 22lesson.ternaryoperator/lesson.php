47816f614e9c80786cc92f914a625012a138daa9
misha366 <misha.moskalenko.jv@gmail.com>
2025-01-09 02:02:10 +0300

    added 22 lesson

---
+++ 22lesson.ternaryoperator/lesson.php
@@ -0,0 +1,17 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title><?= "Teranry Operator"; ?></title>
+</head>
+<body>
+    <?php
+        // condition ? expression1 : expression2
+
+        // более лаконичный вариант оператора if
+        // возвращает результат вычисления expression1/expression2
+        // (в зависимости от condition) 
+    ?>
+</body>
+</html>

b1d2e84be24a5724531b6115aba89336548b0092
misha366 <misha.moskalenko.jv@gmail.com>
2025-01-09 01:52:43 +0300

    added 21 lesson

---
+++ 21lesson.logicaloperators/lesson.php
@@ -0,0 +1,17 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title><?= "Logical Operators"; ?></title>
+</head>
+<body>
+    <?php
+        // &&, ||, !
+
+        // В php набор логических операторов стандартный, все операторы
+        // работают так же как и в других языках и не имеют специфичных
+        // особенностей
+    ?>
+</body>
+</html>

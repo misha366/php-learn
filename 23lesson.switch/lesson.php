506d70bb84fcf6a7b02f3c36e252a05c2f94d14b
misha366 <misha.moskalenko.jv@gmail.com>
2025-01-09 13:15:46 +0300

    added 23 lesson

---
+++ 23lesson.switch/lesson.php
@@ -0,0 +1,26 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title><?= "Switch"; ?></title>
+</head>
+<body>
+    <?php
+        // Структура конструкции switch:
+        // switch ($variable) {
+        //     case 'value':
+        //         # code...
+        //         break;
+            
+        //     case 'value2':
+        //         # code...
+        //         break;
+                
+        //     default:
+        //         # code...
+        //         break;
+        // }
+    ?>
+</body>
+</html>

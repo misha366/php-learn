6055d5a4576f9ebb449dad2e28e73043097f01c8
misha366 <misha.moskalenko.jv@gmail.com>
2025-01-17 21:51:57 +0300

    solved tasks in 41 lesson

--- 41lesson.functions4/lesson.php
+++ 41lesson.functions4/lesson.php
@@ -27,6 +27,31 @@
         $message = "foo";
 
         $sample();
+        echo "<br/><br/>Tasks: <br/>";
+
+        // 1 Ф-я с проверкой на чётность
+
+        // 1.1
+        function isEven(int $number): bool {
+            return !($number % 2);
+        }
+        echo "<hr/>Task 1.1: <br/>";
+        echo isEven(2) . "<br/>";
+        echo isEven(543) . "<br/>";
+        echo isEven(9999) . "<br/>";
+        echo isEven(1918) . "<hr/>";
+
+        // Сумма всех аргументов
+
+        // 2.1
+        echo "Task 2.1: <br/>";
+
+        function sumArgs(int ...$args) : int {
+            return array_sum($args);
+        }
+
+        echo sumArgs(33, 32, 599, 283832, 100, 0, 1, 2);
+
     ?>
 </body>
 </html>
